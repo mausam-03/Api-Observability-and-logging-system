79047d279f011886be8868e6b6d7c7781bb9dd97
Mausam Jha <mausamjha2503@gmail.com>
2026-03-05 23:37:31 +0530

    the logs are getting saved in a file along with timestamp

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -5,7 +5,8 @@ const logger = winston.createLogger({
     level: "info",
     format: combine(timestamp(),json()),  //added timestamp
     transports:[
-        new winston.transports.Console()
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "src/logs/app.log" })   //adding logs in file app.log
     ]
 });
 
