006167cdf593e055b2bdf079459023c249aaa31b
Mausam Jha <mausamjha2503@gmail.com>
2026-03-22 11:54:03 +0530

    added log retention policy

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -2,20 +2,25 @@ import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";  
 
 const { combine, timestamp, json } = winston.format;
+
 const logger = winston.createLogger({
-    level: "info",
-    format: combine(timestamp(),json()),  //added timestamp
-    transports:[
-        new winston.transports.Console(),
-        //new winston.transports.File({ filename: "src/logs/app.log" }) adding logs in file app.log
-        //adding daily rotation to logs
-        new DailyRotateFile({
-               filename: "logs/application-%DATE%.log",
+  level: "info",
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.Console(),
+
+    new DailyRotateFile({
+      filename: "./logs/application-%DATE%.log",
       datePattern: "YYYY-MM-DD",
+
+      // Retention policies
       maxSize: "20m",
-      maxFiles: "14d"
+      maxFiles: "7d",
+      zippedArchive: true,
+
+      level: "info"
     })
-    ]
+  ]
 });
 
 export default logger;
